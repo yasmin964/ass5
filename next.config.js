36d672244e5961da8ebc54e55f320af53b5280a6
yasmin964 <114652589+yasmin964@users.noreply.github.com>
2024-07-10 10:13:43 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-const nextConfig = {};
+// next.config.js
+const sveltePreprocess = require('svelte-preprocess');
 
-export default nextConfig;
+module.exports = {
+  webpack: (config) => {
+    config.resolve.extensions.push('.svelte');
+    config.module.rules.push({
+      test: /\.svelte$/,
+      use: {
+        loader: 'svelte-loader',
+        options: {
+          preprocess: sveltePreprocess(),
+        },
+      },
+    });
+    return config;
+  },
+};
